fbd4cee36ce1f32f0431a6d980d984aac9adca54
Amal Atmani <amalatmani@outlook.com>
2026-07-02 15:35:23 +0100

    Add second NAT Gateway for full multi-AZ fault tolerance

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -44,19 +44,21 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_eip" "nat" {
+  count  = var.az_count
   domain = "vpc"
 
   tags = {
-    Name = "ecs-staff-portal-nat-eip"
+    Name = "ecs-staff-portal-nat-eip-${count.index + 1}"
   }
 }
 
 resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  count         = var.az_count
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "ecs-staff-portal-nat"
+    Name = "ecs-staff-portal-nat-${count.index + 1}"
   }
 
   depends_on = [aws_internet_gateway.main]
@@ -76,15 +78,16 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
+  count  = var.az_count
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
   }
 
   tags = {
-    Name = "ecs-staff-portal-private-rt"
+    Name = "ecs-staff-portal-private-rt-${count.index + 1}"
   }
 }
 
@@ -97,5 +100,5 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private" {
   count          = var.az_count
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
